a4066656c3713797416c29acf86c56b13b9ca704
Winginit2 <119693096+Winginit2@users.noreply.github.com>
2022-12-02 12:20:57 -0500

    Update PrintName.java

--- FixCode/PrintName.java
+++ FixCode/PrintName.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class PrintName {
   public static void main(String[] args) {
-    System.out.printin("Please enter your name!");
+    System.out.println("Please enter your name!");
     String name = new Scanner(System.in).nextLine();
-    System.out.printin("Hello, " + name);
+    System.out.println("Hello, " + name);
   }
 }
